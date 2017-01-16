96c1b0d737c2852c4b579e538b42b80051779c6b
Pavel Korshunov <korsun4@mail.ru>
2017-01-17 00:13:14 +0300

    first commit

---
+++ parallax.js
@@ -0,0 +1,65 @@
+/*!
+ * parallax.js v1 (https://github.com/PavelKorshunov)
+ * @copyright 2017 Pavel Korshunov
+ * use class="parallax" data-image-src="path/to/images.jpg"
+ */
+
+(function($) {
+
+	$.fn.parallaxInit = function(option) {
+		
+		// setting for speed parallax
+
+		var settings = $.extend({
+			"speed" : "2"
+		}, option);
+
+		return this.each(function(){
+
+			var globalHeight = $(window).height();
+
+			//style of parallax
+
+			$(this).css({
+				"height" : globalHeight,
+				"min-height" : "500px",
+				"position" : "relative",
+				"overflow" : "hidden"
+			})
+			.wrapInner("<div class='content-parallax'>")
+			.prepend("<div class='image-parallax' style='background-image: url("+ $(this).data('image-src') +"); background-size:cover; background-position:top; position:absolute; top:0; width:100%;'>");
+		
+			// function for work of parallax
+
+			function parallaxUse() {
+
+				var myheight = $(this).height();
+				var thisImgClass = $(".parallax").find(".image-parallax");
+				thisImgClass.css({
+					"height" : myheight,
+					"min-height" : "500px",
+				});
+
+			var st = $(this).scrollTop();
+			var oneHeight = globalHeight/100; // find 1%
+			
+				thisImgClass.css({
+				"transform" : "translate3d(0px, " + st/oneHeight/settings.speed + "%, 0px)",
+				"-webkit-transform" : "translate3d(0px, " + st/oneHeight/settings.speed + "%, 0px)"
+				});
+
+			};
+
+			$(window).scroll(function() {
+				parallaxUse();
+			}).load(function() {
+				parallaxUse();
+			});
+			$("*").resize(function(){
+				parallaxUse();
+			});
+
+		});
+	};
+
+})(jQuery);
